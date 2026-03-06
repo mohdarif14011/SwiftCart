7780ca536b9a6609b0026eaeafa945e71a3e0b98
Mohd Arif <mohdarif.12012@gmail.com>
2026-03-06 17:52:21 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-7163988392-c8df1",
+  "appId": "1:246775187140:web:0e92214fa617d2c9116b91",
+  "apiKey": "AIzaSyCxzBgnil4Ub5NRQnDue56RaraQFvuyjz0",
+  "authDomain": "studio-7163988392-c8df1.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "246775187140"
+};
