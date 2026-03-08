2ba6e19777d5fe1e30963bea4806bac3378702ef
Mohd Arif <mohdarif.12012@gmail.com>
2026-03-08 09:47:04 +0000

    Still there is random image, remove the placeholder from the product ima

--- next.config.ts
+++ next.config.ts
@@ -13,22 +13,16 @@ const nextConfig: NextConfig = {
     remotePatterns: [
       {
         protocol: 'https',
-        hostname: 'placehold.co',
-        port: '',
-        pathname: '/**',
-      },
-      {
-        protocol: 'https',
-        hostname: 'picsum.photos',
-        port: '',
-        pathname: '/**',
-      },
-      {
-        protocol: 'https',
         hostname: 'res.cloudinary.com',
         port: '',
         pathname: '/**',
       },
+      {
+        protocol: 'https',
+        hostname: 'placehold.co',
+        port: '',
+        pathname: '/**',
+      }
     ],
   },
 };
